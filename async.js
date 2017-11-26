00718bcebb96714c86d1b547096aea689d538581
Dan1elNeal <ishindanil96@gmail.com>
2017-11-27 03:12:58 +0500

    3

--- async.js
+++ async.js
@@ -6,26 +6,28 @@ exports.runParallel = runParallel;
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
+function runParallel(jobs, parallelNum, timeout = 9000) {
     return new Promise((resolve, reject) => {
         if (jobs.length === 0) {
             resolve([]);
         }
-        
+
         let result = []
         let lastJobIndex = parallelNum - 1;
+
+        let jobsWithTimeouts = jobs.map(job => [
+            job(), 
+            new Promise((reject) => {
+                setTimeout(reject, timeout, new Error(`Promise timeout`));
+            })
+        ])
 
         jobs.slice(0, parallelNum).forEach((_, index) => {
             startJob(index);
         });
         
         function startJob(jobIndex) {
-            Promise.race([
-                jobs[jobIndex](),
-                new Promise((reject) => {
-                    setTimeout(reject, timeout, new Error(`Promise timeout`));
-                })
-            ])
+            Promise.race(jobsWithTimeouts[jobIndex])
                 .then(jobResult => finishJob(jobResult, jobIndex));
         }
 
