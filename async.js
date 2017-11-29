0bb0cff250cb1bcda124490b4402fe02b3a04737
Dan1elNeal <ishindanil96@gmail.com>
2017-11-29 23:17:36 +0500

    исправил замечания

--- async.js
+++ async.js
@@ -1,3 +1,5 @@
+'use strict';
+
 exports.isStar = true;
 exports.runParallel = runParallel;
 
@@ -5,32 +7,35 @@ exports.runParallel = runParallel;
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ * @returns {Promise}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
         if (jobs.length === 0) {
             resolve([]);
+
+            return;
         }
-        
-        let result = []
+
+        let result = [];
         let lastJobIndex = parallelNum - 1;
 
-        let jobsWithTimeouts = jobs.map(job =>{
+        let jobsWithTimeouts = jobs.map(job => {
             return {
-                job: job,
-                timeoutPromise: new Promise((_, reject) => {
-                    setTimeout(reject, timeout, new Error(`Promise timeout`));
+                jobPromise: job(),
+                timeoutPromise: new Promise((timeoutResolve, timeoutReject) => {
+                    setTimeout(timeoutReject, timeout, new Error('Promise timeout'));
                 })
-            }
-        })
+            };
+        });
 
         for (let index = 0; index < parallelNum; index++) {
             startJob(index);
         }
-        
+
         function startJob(jobIndex) {
             Promise.race([
-                jobsWithTimeouts[jobIndex].job(),
+                jobsWithTimeouts[jobIndex].jobPromise,
                 jobsWithTimeouts[jobIndex].timeoutPromise
             ])
                 .then(jobResult => finishJob(jobResult, jobIndex))
@@ -39,13 +44,13 @@ function runParallel(jobs, parallelNum, timeout = 1000) {
 
         function finishJob(jobResult, jobIndex) {
             result[jobIndex] = jobResult;
-            if (lastJobIndex != jobs.length - 1){
+            if (lastJobIndex !== jobs.length - 1) {
                 lastJobIndex++;
                 startJob(lastJobIndex);
             }
-            if (result.length == jobs.length) {
+            if (result.length === jobs.length) {
                 resolve(result);
             }
         }
-    })
+    });
 }
